fbd04947120d5295d338298e866d2aa88958856c
kavos113 <kohei.hadano@gmail.com>
2025-12-03 16:23:22 +0900

    window message

---
+++ win32/04_window_message/Application.h
@@ -0,0 +1,31 @@
+#ifndef WIN32_02_SPLIT_CLASS_APPLICATION_H
+#define WIN32_02_SPLIT_CLASS_APPLICATION_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+class Application
+{
+public:
+    Application();
+    ~Application();
+
+    int createWindow(int x = CW_USEDEFAULT, int y = CW_USEDEFAULT, int width = 800, int height = 600);
+
+    void run();
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    LRESULT handleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    HWND m_hwnd;
+
+    const wchar_t* className = L"ApplicationWindowClass";
+};
+
+
+
+#endif //WIN32_02_SPLIT_CLASS_APPLICATION_H

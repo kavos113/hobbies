fb2cfa87bdbe0c57868212672ae695b247966aee
kavos113 <kohei.hadano@gmail.com>
2026-01-03 19:34:10 +0900

    fix tokenizer

--- low-level/compiler/c-compiler/src/token.c
+++ low-level/compiler/c-compiler/src/token.c
@@ -99,7 +99,7 @@ Token *tokenize(char *p)
       continue;
     }
 
-    if (strchr("+-*/()<>", *p))
+    if (strchr("+-*/()<>;", *p))
     {
       current = new_token(TK_RESERVED, current, p++, 1);
       continue;
--- low-level/compiler/c-compiler/test.sh
+++ low-level/compiler/c-compiler/test.sh
@@ -16,14 +16,14 @@ assert() {
     fi
 }
 
-assert 34 "10-4+28"
-assert 18 "  29 - 10+ 4  - 5"
-assert 13 "3 + 2*5"
-assert 39 "(3 + 10) * (4 - 1)"
-assert 4 "(6 + 2 * 3) / 3"
-assert 5 "-10 + 15"
-assert 6 "-3 * (-2)"
-assert 1 "3 + 5 == 8"
-assert 0 "4 * 3 + 2 * (1+5) > 100"
+assert 34 "10-4+28;"
+assert 18 "  29 - 10+ 4  - 5;"
+assert 13 "3 + 2*5;"
+assert 39 "(3 + 10) * (4 - 1);"
+assert 4 "(6 + 2 * 3) / 3;"
+assert 5 "-10 + 15;"
+assert 6 "-3 * (-2);"
+assert 1 "3 + 5 == 8;"
+assert 0 "4 * 3 + 2 * (1+5) > 100;"
 
 echo OK.

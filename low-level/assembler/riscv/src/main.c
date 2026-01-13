8993a92d365920cf0489a94e30be469a593e6ddf
kavos113 <kohei.hadano@gmail.com>
2026-01-13 14:32:57 +0900

    file output

--- low-level/assembler/riscv/src/main.c
+++ low-level/assembler/riscv/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "tokenizer.h"
 #include "parser.h"
@@ -9,20 +10,24 @@ int main(int argc, char **argv)
   char *program = "add t0, t0, t1\n";
   
   char *output_path = "out";
-  // FILE *f = fopen(output_path, "w");
-  // if (!f)
-  // {
-  //   fprintf(stderr, "cannot open file\n");
-  //   exit(1);
-  // }
+  FILE *f = fopen(output_path, "wb");
+  if (!f)
+  {
+    fprintf(stderr, "cannot open file\n");
+    exit(1);
+  }
 
   Token *tok = tokenize(program);
   Instruction *inst = parse(tok);
   while(inst)
   {
     unsigned int result = generate_instruction(inst);
-    printf("%x\n", result);
+    fwrite(&result, sizeof(unsigned int), 1, f);
 
     inst = inst->next;
   }
+
+  fclose(f);
+
+  return 0;
 }

a8330cae474850b1611d2289705713d2036d8282
kavos113 <kohei.hadano@gmail.com>
2026-01-21 16:24:03 +0900

    dwrite glyph run ok

--- graphics/windows/dwrite/03_draw_glyphrun/DWriteEngine.cpp
+++ graphics/windows/dwrite/03_draw_glyphrun/DWriteEngine.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <string>
+#include <vector>
 
 DWriteEngine::DWriteEngine(const Microsoft::WRL::ComPtr<ID2D1RenderTarget>& target)
     : m_renderTarget(target)
@@ -18,22 +18,6 @@ DWriteEngine::DWriteEngine(const Microsoft::WRL::ComPtr<ID2D1RenderTarget>& targ
         throw std::runtime_error("failed to create dwrite factory");
     }
 
-    hr = m_factory->CreateTextFormat(
-        L"Segoe UI",
-        nullptr,
-        DWRITE_FONT_WEIGHT_REGULAR,
-        DWRITE_FONT_STYLE_NORMAL,
-        DWRITE_FONT_STRETCH_NORMAL,
-        32.0f,
-        L"en-US",
-        &m_format
-    );
-    if (FAILED(hr))
-    {
-        std::cerr << "Failed to create text format" << std::endl;
-        throw std::runtime_error("failed to create text format");
-    }
-
     hr = m_renderTarget->CreateSolidColorBrush(
         D2D1::ColorF(D2D1::ColorF::Black),
         &m_brush
@@ -43,16 +27,107 @@ DWriteEngine::DWriteEngine(const Microsoft::WRL::ComPtr<ID2D1RenderTarget>& targ
         std::cerr << "failed to create solid color brush" << std::endl;
         throw std::runtime_error("failed to create solid color brush");
     }
+
+    Microsoft::WRL::ComPtr<IDWriteFontCollection> fontCollection;
+    hr = m_factory->GetSystemFontCollection(&fontCollection);
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to get system font collection" << std::endl;
+        throw std::runtime_error("failed to get system font collection");
+    }
+
+    UINT32 index;
+    BOOL exist;
+    hr = fontCollection->FindFamilyName(L"Segoe UI", &index, &exist);
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to find font family" << std::endl;
+        throw std::runtime_error("failed to find font family");
+    }
+
+    Microsoft::WRL::ComPtr<IDWriteFontFamily> fontFamily;
+    hr = fontCollection->GetFontFamily(index, &fontFamily);
+    if (FAILED(hr))
+    {
+        std::cerr << "failed get font family" << std::endl;
+        throw std::runtime_error("failed get font family");
+    }
+
+    Microsoft::WRL::ComPtr<IDWriteFont> font;
+    hr = fontFamily->GetFirstMatchingFont(
+        DWRITE_FONT_WEIGHT_NORMAL,
+        DWRITE_FONT_STRETCH_NORMAL,
+        DWRITE_FONT_STYLE_NORMAL,
+        &font
+    );
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to get font" << std::endl;
+        throw std::runtime_error("failed to get font");
+    }
+
+    hr = font->CreateFontFace(&m_face);
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to create font face" << std::endl;
+        throw std::runtime_error("failed to create font face");
+    }
 }
 
 void DWriteEngine::draw() const
 {
-    std::wstring string = L"Hello, DirectWrite";
-    m_renderTarget->DrawTextA(
-        string.c_str(),
-        string.size(),
-        m_format.Get(),
-        D2D1::RectF(0, 0, 300, 300),
-        m_brush.Get()
+    UINT32 textLength = static_cast<UINT32>(STRING.size());
+    std::vector<UINT16> glyphIndices(textLength);
+
+    // latin only
+    std::vector<UINT32> codePoints(textLength);
+    for (int i = 0; i < textLength; ++i)
+    {
+        codePoints[i] = STRING[i];
+    }
+
+    HRESULT hr = m_face->GetGlyphIndicesA(codePoints.data(), codePoints.size(), glyphIndices.data());
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to get glyph indices" << std::endl;
+        return;
+    }
+
+    std::vector<DWRITE_GLYPH_METRICS> glyphMetrics(textLength);
+    hr = m_face->GetDesignGlyphMetrics(glyphIndices.data(), textLength, glyphMetrics.data());
+    if (FAILED(hr))
+    {
+        std::cerr << "failed to get glyph metrics" << std::endl;
+        return;
+    }
+
+    DWRITE_FONT_METRICS metrics{};
+    m_face->GetMetrics(&metrics);
+
+    float fontSize = 36.0f;
+    float scale = fontSize / (float)metrics.designUnitsPerEm;
+
+    std::vector<float> glyphAdvances(textLength);
+    for (int i = 0; i < textLength; ++i)
+    {
+        glyphAdvances[i] = glyphMetrics[i].advanceWidth * scale;
+    }
+
+    DWRITE_GLYPH_RUN glyphRun = {
+        .fontFace = m_face.Get(),
+        .fontEmSize = fontSize,
+        .glyphCount = textLength,
+        .glyphIndices = glyphIndices.data(),
+        .glyphAdvances = glyphAdvances.data(),
+        .glyphOffsets = nullptr,
+        .isSideways = FALSE,
+        .bidiLevel = 0
+    };
+
+    m_renderTarget->DrawGlyphRun(
+        D2D1::Point2F(100, 100),
+        &glyphRun,
+        m_brush.Get(),
+        DWRITE_MEASURING_MODE_NATURAL
     );
 }
--- graphics/windows/dwrite/03_draw_glyphrun/DWriteEngine.h
+++ graphics/windows/dwrite/03_draw_glyphrun/DWriteEngine.h
@@ -4,6 +4,7 @@
 #include <wrl/client.h>
 #include <d2d1.h>
 #include <dwrite.h>
+#include <string>
 
 class DWriteEngine
 {
@@ -14,10 +15,12 @@ public:
     void draw() const;
 
 private:
+    const std::wstring STRING = L"Hello, DirectWrite!";
+
     Microsoft::WRL::ComPtr<ID2D1RenderTarget> m_renderTarget;
 
     Microsoft::WRL::ComPtr<IDWriteFactory> m_factory;
-    Microsoft::WRL::ComPtr<IDWriteTextFormat> m_format;
+    Microsoft::WRL::ComPtr<IDWriteFontFace> m_face;
     Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> m_brush;
 };
 

5831063797ecfee39ab41477f6862c3273606c85
kavos113 <kohei.hadano@gmail.com>
2026-02-05 15:04:45 +0900

    fmt

--- lang/react/hello-react/src/SomeComponent.tsx
+++ lang/react/hello-react/src/SomeComponent.tsx
@@ -1,10 +1,8 @@
-export default function SomeComponent(props: {
-    message: string
-}) {
-    return (
-        <div>
-            <h2>{props.message}</h2>    
-            <p>This is some component</p>
-        </div>
-    )
+export default function SomeComponent(props: { message: string }) {
+  return (
+    <div>
+      <h2>{props.message}</h2>
+      <p>This is some component</p>
+    </div>
+  );
 }

c356d517f789a30c852f85b90be1840c89e2e8d0
kavos113 <kohei.hadano@gmail.com>
2026-06-16 08:16:49 +0900

    present init

---
+++ graphics/vulkan/12_present/App.cpp
@@ -0,0 +1,34 @@
+#include "App.h"
+
+App::App()
+    : m_window(nullptr)
+{
+    createWindow();
+
+    m_engine = std::make_unique<VulkanEngine>(m_window);
+}
+
+App::~App()
+{
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+}
+
+void App::run()
+{
+    while (!glfwWindowShouldClose(m_window))
+    {
+        glfwPollEvents();
+        m_engine->render();
+    }
+}
+
+void App::createWindow()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    m_window = glfwCreateWindow(800, 600, "Hello GLFW", nullptr, nullptr);
+}
---
+++ graphics/vulkan/12_present/VulkanEngine.h
@@ -0,0 +1,86 @@
+#ifndef CREATE_INSTANCE_VULKANENGINE_H
+#define CREATE_INSTANCE_VULKANENGINE_H
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include <vulkan/vulkan.h>
+
+#define NOMINMAX
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#include <glfw/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <glfw/glfw3native.h>
+
+#include "VulkanDebug.h"
+
+class VulkanEngine
+{
+public:
+    VulkanEngine(GLFWwindow* window);
+    ~VulkanEngine();
+
+    void render();
+
+private:
+    void createInstance();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createSurface(GLFWwindow* window);
+    void createSwapchain(GLFWwindow* window);
+    void createImageViews();
+    void createPipeline();
+    void createCommandPool();
+    void createCommandBuffer();
+
+    void recordCommandBuffer(uint32_t imageIndex) const;
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat() const;
+    VkPresentModeKHR chooseSwapPresentMode() const;
+    VkExtent2D chooseSwapExtent(GLFWwindow* window) const;
+
+    VkShaderModule createShaderModule(const std::string& filePath) const;
+    void transitionImageLayout(
+        uint32_t imageIndex,
+        VkImageLayout oldLayout,
+        VkImageLayout newLayout,
+        VkAccessFlags2 srcAccessMask,
+        VkAccessFlags2 dstAccessMask,
+        VkPipelineStageFlags2 srcStageMask,
+        VkPipelineStageFlags2 dstStageMask
+    ) const;
+
+    std::unique_ptr<VulkanDebug> m_debug;
+
+    VkInstance m_instance = VK_NULL_HANDLE;
+    VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+    VkDevice m_device = VK_NULL_HANDLE;
+    VkQueue m_graphicsQueue = VK_NULL_HANDLE;
+
+    VkSurfaceKHR m_surface = VK_NULL_HANDLE;
+    VkSwapchainKHR m_swapchain = VK_NULL_HANDLE;
+    std::vector<VkImage> m_swapchainImages;
+    std::vector<VkImageView> m_swapchainImageViews;
+    VkSurfaceFormatKHR m_swapchainImageFormat;
+    VkExtent2D m_swapchainExtent;
+
+    VkViewport m_viewport;
+    VkRect2D m_scissor;
+
+    VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+    VkPipeline m_graphicsPipeline = VK_NULL_HANDLE;
+
+    VkCommandPool m_commandPool = VK_NULL_HANDLE;
+    VkCommandBuffer m_commandBuffer = VK_NULL_HANDLE;
+
+#ifdef NDEBUG
+    const bool m_enableValidationLayers = false;
+#else
+    const bool m_enableValidationLayers = true;
+#endif
+};
+
+
+#endif //CREATE_INSTANCE_VULKANENGINE_H

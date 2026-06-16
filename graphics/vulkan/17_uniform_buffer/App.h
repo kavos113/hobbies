70b4d769220e21bcaa93a75774d3d3d8a281e121
kavos113 <kohei.hadano@gmail.com>
2026-06-16 13:57:15 +0900

    uniform buffer init

---
+++ graphics/vulkan/17_uniform_buffer/App.h
@@ -0,0 +1,29 @@
+#ifndef HELLO_GLFW_APP_H
+#define HELLO_GLFW_APP_H
+
+#include <memory>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "VulkanEngine.h"
+
+class App
+{
+public:
+    App();
+    ~App();
+
+    void run();
+
+private:
+    void createWindow();
+
+    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+
+    GLFWwindow* m_window;
+    std::unique_ptr<VulkanEngine> m_engine;
+};
+
+
+#endif //HELLO_GLFW_APP_H

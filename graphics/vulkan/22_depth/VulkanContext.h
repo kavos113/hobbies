acb343567499e72f8a4fe43f31ab8b4dbb7b3f64
kavos113 <kohei.hadano@gmail.com>
2026-07-19 13:04:55 +0900

    depth init

---
+++ graphics/vulkan/22_depth/VulkanContext.h
@@ -0,0 +1,78 @@
+#ifndef REFACTOR_VULKANCONTEXT_H
+#define REFACTOR_VULKANCONTEXT_H
+
+#include <memory>
+
+#include <vulkan/vulkan.h>
+
+#include "VulkanDebug.h"
+
+class VulkanContext
+{
+public:
+    VulkanContext();
+    ~VulkanContext();
+
+    int findQueueFamilies() const;
+
+    VkCommandBuffer beginSingleTimeCommands() const;
+    void endSingleTimeCommands(VkCommandBuffer commandBuffer) const;
+
+    VkInstance instance() const
+    {
+        return m_instance;
+    }
+
+    VkPhysicalDevice physicalDevice() const
+    {
+        return m_physicalDevice;
+    }
+
+    VkDevice device() const
+    {
+        return m_device;
+    }
+
+    VkQueue graphicsQueue() const
+    {
+        return m_graphicsQueue;
+    }
+
+    VkDescriptorPool descriptorPool() const
+    {
+        return m_descriptorPool;
+    }
+
+    VkCommandPool commandPool() const
+    {
+        return m_commandPool;
+    }
+
+private:
+    void createInstance();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createCommandPool();
+    void createDescriptorPool();
+
+    std::unique_ptr<VulkanDebug> m_debug;
+
+    VkInstance m_instance = VK_NULL_HANDLE;
+    VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+    VkDevice m_device = VK_NULL_HANDLE;
+    VkQueue m_graphicsQueue = VK_NULL_HANDLE;
+
+    VkCommandPool m_commandPool = VK_NULL_HANDLE;
+    VkDescriptorPool m_descriptorPool = VK_NULL_HANDLE;
+
+    static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
+
+#ifdef NDEBUG
+    const bool m_enableValidationLayers = false;
+#else
+    const bool m_enableValidationLayers = true;
+#endif
+};
+
+
+#endif //REFACTOR_VULKANCONTEXT_H
---
+++ graphics/vulkan/22_depth/VulkanEngine.h
@@ -0,0 +1,75 @@
+#ifndef CREATE_INSTANCE_VULKANENGINE_H
+#define CREATE_INSTANCE_VULKANENGINE_H
+
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <vulkan/vulkan.h>
+
+#define NOMINMAX
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#include <glfw/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <glfw/glfw3native.h>
+
+#include "VulkanContext.h"
+#include "Object.h"
+
+class VulkanEngine
+{
+public:
+    VulkanEngine(GLFWwindow* window, VulkanContext *context);
+    ~VulkanEngine();
+
+    void render();
+
+    bool isResized = false;
+
+private:
+    void createSurface();
+    void createSwapchain();
+    void createImageViews();
+    void createPipeline();
+    void createCommandBuffer();
+    void createSyncObjects();
+
+    void recordCommandBuffer(uint32_t imageIndex) const;
+    void recreateSwapchain();
+    void cleanupSwapchain();
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat() const;
+    VkPresentModeKHR chooseSwapPresentMode() const;
+    VkExtent2D chooseSwapExtent(GLFWwindow* window) const;
+
+    VkShaderModule createShaderModule(const std::string& filePath) const;
+
+    VulkanContext *m_context;
+    std::unique_ptr<Object> m_object;
+    GLFWwindow* m_window = nullptr;
+
+    VkSurfaceKHR m_surface = VK_NULL_HANDLE;
+    VkSwapchainKHR m_swapchain = VK_NULL_HANDLE;
+    std::vector<VkImage> m_swapchainImages;
+    std::vector<VkImageView> m_swapchainImageViews;
+    VkSurfaceFormatKHR m_swapchainImageFormat;
+    VkExtent2D m_swapchainExtent;
+
+    VkViewport m_viewport;
+    VkRect2D m_scissor;
+
+    VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+    VkPipeline m_graphicsPipeline = VK_NULL_HANDLE;
+
+    std::vector<VkCommandBuffer> m_commandBuffers;
+    std::vector<VkSemaphore> m_imageAvailableSemaphores;
+    std::vector<VkSemaphore> m_renderCompleteSemaphores;
+    std::vector<VkFence> m_drawFences;
+
+    static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
+    uint32_t m_currentFrame = 0;
+};
+
+
+#endif //CREATE_INSTANCE_VULKANENGINE_H
